17c36fed69994bbda5b6cffa38f699c383d94ec3
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-04-12 22:35:51 -0500

    Add all models to chat admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from chat.models import User, Chat, Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["author", "chat", "send_time"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Chat)
+admin.site.register(Message, MessageAdmin)
