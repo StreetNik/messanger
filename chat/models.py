8893b002f5d7ed579afaaa89edb1bdf766c8ff22
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-04-12 22:29:14 -0500

    Add models for chat app

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from tipa_telegram import settings
+from messanger import settings
 
 
 class User(AbstractUser):
