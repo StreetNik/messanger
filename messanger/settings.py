63e39192c33793233721906ae3d55e7ee0833740
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-04-12 22:20:56 -0500

    Add ASGI & channel layers to settings file

--- messanger/settings.py
+++ messanger/settings.py
@@ -44,6 +44,14 @@ INSTALLED_APPS = [
     "chat",
 ]
 
+ASGI_APPLICATION = "messanger.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
