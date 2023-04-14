85dff1ecbe015c236753db566c1189aa3763cd33
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-04-14 11:00:20 -0500

    Add authentication middleware to settings file

--- messanger/settings.py
+++ messanger/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy, reverse
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "middleware.authentication_middleware.AuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = 'messanger.urls'
